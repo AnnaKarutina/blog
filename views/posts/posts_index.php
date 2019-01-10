f0d438c3f4823db9afc853babb059b03b7a0d5ac
AnnaKarutina <anna.karutina@khk.ee>
2019-01-10 08:18:33 +0200

    Add single post viewing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,6 +1,6 @@
 <?foreach($posts as $post):?>
     <div class="span8">
-        <h1><? echo $post['post_subject']?></h1>
+        <h1><a href="<? echo BASE_URL?>posts/view/<?echo $post['post_id']?>"><?echo $post['post_subject']; ?></a></h1>
         <p><? echo $post['post_text']?></p>
         <div>
             <span class="badge badge-success">Posted <?php echo $post['post_created']; ?></span><div class="pull-right"><span class="label label-primary">alice</span> <span class="label label-primary">story</span> <span class="label label-primary">blog</span> <span class="label label-primary">personal</span></div>
